661cee8a0ad820ac1bcd084d6f652629d4a13072
jaymzolali <jaymz.olali@gmail.com>
2024-04-22 19:18:55 +0100

    second

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -42,7 +42,7 @@ const Page = () => {
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.207303376754!2d-0.14593422428855815!3d51.50941267181374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487605298661c0eb%3A0xd4961a9fe0c684ae!2s1%20Grafton%20St%2C%20London%20W1S%204FE%2C%20UK!5e0!3m2!1sen!2sng!4v1709830904368!5m2!1sen!2sng" 
                     width="359" 
                     height="200" 
-                    style={{border: "0"}} 
+                    style={{border: '0'}} 
                     allowfullscreen="" 
                     loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
