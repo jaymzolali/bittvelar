627fa3ff685d35762c3062082d77fcb0f27b934c
jaymzolali <jaymz.olali@gmail.com>
2024-04-22 19:03:02 +0100

    first

---
+++ src/app/convert/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='mt-20'>Convert</div>
+  )
+}
+
+export default Page
---
+++ src/app/transfer/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='mt-20'>Transfer</div>
+  )
+}
+
+export default Page
